58d14775949688c5814ffe18e94de5b78061e949
dlstonedl <ldeng@thoughtworks.com>
2019-04-09 21:11:54 +0800

    use datasource to get connect

--- src/main/java/com/dlstone/mybatis/analysis/jdbc/JdbcDemo.java
+++ src/main/java/com/dlstone/mybatis/analysis/jdbc/JdbcDemo.java
@@ -1,19 +1,24 @@
 package com.dlstone.mybatis.analysis.jdbc;
 
 import com.dlstone.mybatis.analysis.sample.User;
+import org.apache.commons.dbcp2.BasicDataSourceFactory;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Properties;
 
 public class JdbcDemo {
 
     public static void main(String[] args) throws Exception{
-        Connection connection = DriverManager.getConnection(
-                        "jdbc:mysql://localhost:3306/mybatis_analysis",
-                        "root",
-                        "");
+        Properties properties = new Properties();
+        properties.setProperty("driverClassName", "com.mysql.jdbc.Driver");
+        properties.setProperty("url", "jdbc:mysql://localhost:3306/mybatis_analysis");
+        properties.setProperty("username", "root");
+        properties.setProperty("password", "");
+        DataSource dataSource = BasicDataSourceFactory.createDataSource(properties);
+        Connection connection = dataSource.getConnection();
 
         PreparedStatement preparedStatement = connection.prepareStatement(
                 "select * from user where id = ?");
